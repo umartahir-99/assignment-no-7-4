d9d22bdf1630169fe5a071c0adf6f67faf65ca50
umartahir-99 <tahirumar835@gmail.com>
2024-12-21 20:17:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+
+
+// Objective: Simulate a simple game.
+// Task:
+// Accept inputs for two players (rock, paper, or scissors).
+// Use if conditions to determine the winner:
+// Rock beats Scissors
+// Paper beats Rock
+// Scissors beats Paper
+// Print "Player 1 Wins," "Player 2 Wins," or "It's a Tie!"
+
+
+
+var P1 = prompt("Enter Rock, Paper or Scissors");
+var P2 = prompt("Enter Rock, Paper or Scissors");
+if("P1 == Rock && P2 == Scissors"){
+  console.log("P1 wins")
+
+}
+else if("P1 == Paper && P2 == Rock"){
+  console.log("P1 wins")
+
+}
+else{
+  console.log("P2 wins")
+}
+
+
+
